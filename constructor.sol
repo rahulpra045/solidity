f0014025a6f9947c51d89ce60f306f635f3a05d4
rahulpra045 <98214910+rahulpra045@users.noreply.github.com>
2022-05-22 12:49:57 +0530

    Create constructor.sol

---
+++ constructor.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract local{
+    uint public count;
+
+    constructor(uint new_count)
+    {
+        count = new_count;
+    }
+
+
+}
+//we have to pass argument during deploying the contract
