63bbe9335363c86787108319343eb266066894ac
rahulpra045 <98214910+rahulpra045@users.noreply.github.com>
2022-05-24 12:11:42 +0530

    Create loop.sol

---
+++ loop.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract Loop
+{
+    uint[3] public arr;
+    uint public count;
+
+    function loop() public
+    {
+        // while(count<arr.length){
+        //     arr[count] = count;
+        //     count++;
+        // }
+
+        // for(uint i =count;i<arr.length;i++)
+        // {
+        //     arr[count] = count;
+        //     count++;
+        // }
+
+        do{
+            arr[count] = count;
+            count++;
+        }while(count<arr.length);
+    }
+}
